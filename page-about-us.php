e7503f5323476eba8e215401867bec4cebe4fe71
Milan Poznan <poznan.milan@gmail.com>
2018-12-25 16:56:20 +0100

    Component in progres

--- page-about-us.php
+++ page-about-us.php
@@ -1,7 +1,19 @@
-<?php get_header(); ?>
+<?php get_header();
+
+if( have_rows('content_component') ):
+     // loop through the rows of data
+    while ( have_rows('content_component') ) : the_row();
+    ?>
 
 <div class="about-us">
-  <?php include get_template_directory() . '/template-parts/general-hero.php'; ?> 
+  <?php include get_template_directory() . '/template-parts/general-hero.php'; ?>
+</div>
+<div class="info-image">
+  <?php include get_template_directory() . '/template-parts/info-image-section.php'; ?>
+
 </div>
 
-<?php get_footer(); ?>
+<?php
+endwhile;
+endif;
+ get_footer(); ?>
--- template-parts/general-hero.php
+++ template-parts/general-hero.php
@@ -1,7 +1,8 @@
+<?php if( get_row_layout() == 'general_hero' ): ?>
 <section class="g-hero">
-<?php if( get_field('background_image') ): ?>
-  <div class="g-hero__wrapper g-hero__background" style="background-image: url('<?php the_field('background_image'); ?>')">
-<?php endif; ?>
+<?php //if( _sub_field('background_image') ): ?>
+  <div class="g-hero__wrapper g-hero__background" style="background-image: url('<?php the_sub_field('background_image'); ?>')">
+<?php //endif; ?>
     <div class="g-hero__shape">
       <h1 class"g-hero__title"><?php the_title(); ?></h1>
     </div>
@@ -10,7 +11,8 @@
     <span>SCROLL</span>
   </div>
   <div class="g-hero__side-scroll-text">
-    <?php the_field('side_description') ?>
+    <?php the_sub_field('side_description') ?>
   </div>
 
 </section>
+<?php endif; ?>
