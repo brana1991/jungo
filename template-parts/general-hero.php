d75a60d55ec8b8e95e5d4353651d9f69dadc069a
Milan Poznan <poznan.milan@gmail.com>
2019-01-12 13:52:28 +0100

    Hero is almost done

--- template-parts/general-hero.php
+++ template-parts/general-hero.php
@@ -3,8 +3,8 @@
 <?php //if( _sub_field('background_image') ): ?>
   <div class="g-hero__wrapper g-hero__background" style="background-image: url('<?php the_sub_field('background_image'); ?>')">
 <?php //endif; ?>
-    <div class="g-hero__shape">
-      <h1 class"g-hero__title"><?php the_title(); ?></h1>
+    <div class="g-hero__shape js-image-tilt" data-tilt style="background-image: url('<?php the_sub_field('shape_object_image'); ?>')">
+      <h1 class"g-hero__title js-title-tilt" data-tilt><?php the_title(); ?></h1>
     </div>
   </div>
   <div class="g-hero__side-scroll">
