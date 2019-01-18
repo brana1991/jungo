c02bf41c6710442136d97ececeec4861d3caea75
brana1991 <djbane91@gmail.com>
2019-01-18 18:04:26 +0100

    clients final

--- template-parts/clients-component.php
+++ template-parts/clients-component.php
@@ -1,13 +1,13 @@
 <?php if (have_rows('our_client_repeater')): ?>
 <section class="clients">
-    <div class="clients__row">
+      <ul class="clients__wrap">
     <?php while ( have_rows('our_client_repeater') ) : the_row(); ?>
-      <div class="clients__wrap">
-        <a href="<?php the_sub_field('our_client_url'); ?>">
-          <div class="clients__single-partner" style="background-image: url('<?php the_sub_field('our_client_image'); ?>')"></div>
-        </a>
-      </div>
+        <li>
+          <a href="<?php the_sub_field('our_client_url'); ?>">
+            <div class="clients__single-partner" style="background-image: url('<?php the_sub_field('our_client_image'); ?>')"></div>
+          </a>
+      </li>
     <?php endwhile; ?>
-  </div>
+      </ul>
 </section>
 <?php endif; ?>
