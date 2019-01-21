9582d7150d3baef3cacff76d8bd0ddefe5f54196
Milan Poznan <poznan.milan@gmail.com>
2019-01-21 17:46:50 +0100

    in progres

--- assets/js/src/page-specific-scripts/tilt-efects.js
+++ assets/js/src/page-specific-scripts/tilt-efects.js
@@ -1,10 +1,25 @@
-if (window.innerWidth > 1200) {
-	$('.js-tilt').tilt({
-		glare: true,
-		maxGlare: 0.5,
-	});
+if (window.innerWidth > 1200 && window.innerWidth < 1800) {
+	// $('.js-tilt').tilt({
+	// 	glare: true,
+	// 	maxGlare: 0.5,
+	// });
 	const $imageTilt = $('.js-image-tilt');
 	const $titleTilt = $('.js-image-tilt');
+	$('.g-hero__background').tilt({
+		maxTilt: 5,
+		perspective: 1400,
+		easing: 'cubic-bezier(.03,.98,.52,.99)',
+		speed: 1100,
+		scale: 1.01,
+	});
+	$imageTilt.tilt({
+		maxTilt: 11,
+		perspective: 1000,
+		easing: 'cubic-bezier(.03,.98,.52,.99)',
+		speed: 1400,
+		scale: 1.01,
+	});
+
 	// $imageTilt.tilt({
 	// 	// glare: true,
 	// 	maxTilt: 10,
@@ -13,6 +28,21 @@ if (window.innerWidth > 1200) {
 	// $imageTilt.on('tilt.mouseEnter', function(e) {
 	// $titleTilt.tilt({ maxTilt: 5 });
 	// });
+} else if (window.innerWidth >= 1800) {
+	$('.g-hero__background').tilt({
+		maxTilt: 3,
+		perspective: 600,
+		easing: 'cubic-bezier(.03,.98,.52,.99)',
+		speed: 1000,
+		scale: 1.01,
+	});
+	$imageTilt.tilt({
+		maxTilt: 11,
+		perspective: 1000,
+		easing: 'cubic-bezier(.03,.98,.52,.99)',
+		speed: 1400,
+		scale: 1.01,
+	});
 }
 
 // $('.multiple-items').slick({
